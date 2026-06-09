67ef84261bcca12251f7bd8547b40b105dc5302d
yesim99 <yesimyurdaka1999@outlook.com>
2026-06-09 10:14:41 +0000

    Deploying to gh-pages from @ yesim99/goit-js-hw-10@32b1ae2947685b6a0ae1895213ab6a9dc241b858 🚀

---
+++ 02-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",o=>{o.preventDefault();const t=Number(s.elements.delay.value),r=s.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(t):m(t)},t)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=02-snackbar.js.map
